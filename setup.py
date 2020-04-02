dac4d564b6cdb655d722e42bd156ae83bd132f40
Felix Stupp <felix.stupp@outlook.com>
2020-04-02 10:55:07 +0000

    Configured project to be a python module

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tinytinypy",
+    version='0.1',
+    author="Felix Stupp",
+    author_email="felix.stupp@outlook.com",
+    description="Utility package for using API of Tiny Tiny RSS",
+    long_description=long_description,
+    url="https://github.com/Zocker1999NET/tinytinypy",
+    python_requires='>=3.4',
+    scripts=[
+        "scripts/tinytinypy",
+    ],
+    packages=[
+        "tinytinypy",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+    ],
+)
